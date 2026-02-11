e2175359031850ad51b38b97fcf77e90d49dfaf2
Kashif Bashir <kashif@Kashifs-MacBook-Air.local>
2026-02-11 10:58:08 +0500

    Include Font Awesome via web_include_css

--- euro_website/hooks.py
+++ euro_website/hooks.py
@@ -40,6 +40,7 @@ website_context = {}
 # Website assets
 web_include_css = [
     "/assets/euro_website/css/euro_website.css",
+    "/assets/frappe/css/font-awesome.css",
 ]
 web_include_js = [
     "/assets/euro_website/js/site.js",
